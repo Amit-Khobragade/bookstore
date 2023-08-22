b569fce22221e6024965e611af80011d8d450622
Amit Khobragade <amitkh1102@outlook.com>
2023-08-22 18:26:30 +0530

    Added all the user endpoints

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post, Put } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller({ path: 'user', version: '1' })
+export class UserController {
+  @Get()
+  getUser(@Headers() headers) {
+    console.debug(headers);
+    return {};
+  }
+
+  @Post('sign-in')
+  login(@Body() body) {
+    console.debug(body);
+    return {};
+  }
+
+  @Post('sign-up')
+  signup(@Body() body) {
+    console.debug(body);
+    return {};
+  }
+
+  @Put()
+  updateUser(@Body() body) {
+    console.debug(body);
+    return {};
+  }
+
+  @Put('forgot-password')
+  forgetPassword(@Body() body) {
+    console.debug(body);
+    return {};
+  }
+
+  @Put('update-password')
+  updatePassword(@Body() body) {
+    console.debug(body);
+    return {};
+  }
+}

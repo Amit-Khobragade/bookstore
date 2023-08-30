5ecbdad4ee6e31f9a4d15ca55bb7aa7798d88e9e
Amit Khobragade <amitkh1102@outlook.com>
2023-08-30 21:01:47 +0530

    Added password hashing and JWT to user signup
    This ensure the safety of the password and an easy access to the users
    using access tokens. using bcrypt for hashing the password. Also using
    jsonwebtoken for the creation of JWT

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,22 +1,46 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { UserFirestoreService } from 'src/firebase/user-firestore/user-firestore.service';
-import { UserSignUpRequest } from './DTO/User.dto';
+import { UserInfoResponseDTO, UserSignUpRequest } from './DTO/User.dto';
 import { genSaltSync, hashSync } from 'bcryptjs';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { sign } from 'jsonwebtoken';
+
+const key = readFileSync(resolve(__dirname, '../assets/private_key.pem'), {
+  encoding: 'utf-8',
+}).toLocaleString();
 
 @Injectable()
 export class UserService {
   constructor(private userFirestoreService: UserFirestoreService) {}
-  async signUp(userData: UserSignUpRequest) {
+
+  async signUp(userData: UserSignUpRequest): Promise<UserInfoResponseDTO> {
+    // Checking if the user already exists
     const user = await this.userFirestoreService.getUserByEmail(userData.email);
 
     if (user) {
       throw new ConflictException('user already exists');
     }
 
+    // Hashing password
     const salt = genSaltSync();
     const password = hashSync(userData.password, salt);
-    return this.userFirestoreService.createUser(
-      Object.assign({}, userData, { password }),
+
+    // Creating the user and deleting the password
+    const createdUser = await this.userFirestoreService.createUser(
+      Object.assign({}, userData, { password, isAuthorized: false }),
+    );
+    delete createdUser.password;
+
+    // Creating a jwt and returning that to the user instead of the user Object
+    return new UserInfoResponseDTO(
+      Object.assign(createdUser, {
+        key:
+          'Bearer ' +
+          sign(createdUser, key, { algorithm: 'RS256', expiresIn: '4d' }),
+      }),
     );
   }
+
+  async signIn() {}
 }

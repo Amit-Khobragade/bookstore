5366e9045f3220ca8c7689ea2a251a492ee3a817
Amit Khobragade <amitkh1102@outlook.com>
2023-08-30 21:00:09 +0530

    Added Class Interceptor for serializing classes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { FirebaseModule } from './firebase/firebase.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [UserModule, FirebaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+  ],
 })
 export class AppModule {}

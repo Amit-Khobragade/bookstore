8ad7f385dcac0bc80dca839a17486a82072973ab
Amit Khobragade <amitkh1102@outlook.com>
2023-08-30 13:05:17 +0530

    Added the logic for the signup method
    Now the service checks if the email exists and if it doesn't then
    creates a new user. Also, added the user-firebase service imported as it
    helps in the interaction with the firestore.

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UserFirestoreService } from 'src/firebase/user-firestore/user-firestore.service';
+import { UserSignUpRequest } from './DTO/User.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private userFirestoreService: UserFirestoreService) {}
+  async signUp(userData: UserSignUpRequest) {
+    const user = await this.userFirestoreService.getUserByEmail(userData.email);
+
+    if (user) {
+      throw new ConflictException('user already exists');
+    }
+    return this.userFirestoreService.createUser(Object.assign({}, userData));
+  }
+}

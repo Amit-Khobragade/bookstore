48916718b54216710ec0c6989b825195a2b0505c
Amit Khobragade <amitkh1102@outlook.com>
2023-08-31 18:31:52 +0530

    Added update user service
    This handles the bussiness logic behind updating the user.

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -6,13 +6,15 @@ import {
 import { UserFirestoreService } from 'src/firebase/user-firestore/user-firestore.service';
 import {
   UserInfoResponseDTO,
+  UserJwtPayload,
   UserSignInRequest,
   UserSignUpRequest,
+  UserUpdateRequest,
 } from './DTO/User.dto';
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
-import { sign } from 'jsonwebtoken';
+import { decode, sign, verify } from 'jsonwebtoken';
 
 const key = readFileSync(resolve(__dirname, '../assets/private_key.pem'), {
   encoding: 'utf-8',
@@ -43,17 +45,16 @@ export class UserService {
     // Creating a jwt and returning that to the user instead of the user Object
     return new UserInfoResponseDTO(
       Object.assign(createdUser, {
-        key:
-          'Bearer ' +
-          sign(createdUser, key, { algorithm: 'RS256', expiresIn: '4d' }),
+        key: sign(createdUser, key, { algorithm: 'RS256', expiresIn: '4d' }),
       }),
     );
   }
 
   async signIn(request: UserSignInRequest): Promise<UserInfoResponseDTO> {
     // Get the user and if doesn't exists throw a 400
-    // Compare the passwords if doesn't match throw a 400
     const user = await this.userFirestoreService.getUserByEmail(request.email);
+
+    // Compare the passwords if doesn't match throw a 400
     if (!user || !compareSync(request.password, user.password)) {
       throw new BadRequestException('Invalid username/password');
     }
@@ -63,9 +64,44 @@ export class UserService {
 
     return new UserInfoResponseDTO(
       Object.assign(user, {
-        key:
-          'Bearer ' + sign(user, key, { algorithm: 'RS256', expiresIn: '4d' }),
+        key: sign(user, key, { algorithm: 'RS256', expiresIn: '4d' }),
       }),
     );
+  }
+
+  async updateUser(
+    updatedFields: UserUpdateRequest,
+    authKey: string,
+  ): Promise<UserInfoResponseDTO> {
+    try {
+      // Get the user from the JWT if not valid throw a 400
+      const token = verify(authKey, key, {
+        algorithms: ['RS256'],
+      }) as UserJwtPayload;
+
+      // use the jwt to recieve the user from the database
+      const user = await this.userFirestoreService.getUserById(token.id);
+
+      // if the user doesn't exists throw a 400
+      if (!user) {
+        throw new BadRequestException();
+      }
+
+      // if the user exists in the databse update the user and return a new JWT
+      const updatedUser = await this.userFirestoreService.updateUser(user.id, {
+        ...updatedFields,
+      });
+      delete updatedUser.password;
+
+      // Creating a jwt and returning that to the user instead of the user Object
+      return new UserInfoResponseDTO(
+        Object.assign(updatedUser, {
+          key: sign(updatedUser, key, { algorithm: 'RS256', expiresIn: '4d' }),
+        }),
+      );
+    } catch (err) {
+      console.error(err);
+      throw new BadRequestException('invalid request');
+    }
   }
 }

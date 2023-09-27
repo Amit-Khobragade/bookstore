251aba4f27619de053a6b203136b99fb80b06770
Amit Khobragade <amitkh1102@outlook.com>
2023-09-27 21:10:51 +0530

    Added Order utils
    These consists of an order class, and an order status enum.

--- backend/src/common/OrderDetails.ts
+++ backend/src/common/OrderDetails.ts
@@ -23,3 +23,43 @@ export class Address {
     return Object.assign({}, this);
   }
 }
+
+export enum OrderStatus {
+  'PENDING',
+  'CONFIRMED',
+  'SHIPPED',
+  'RECIEVED',
+  'CANCELLED',
+}
+
+export class Order {
+  constructor(
+    public id: string,
+    public product: string,
+    public quantity: number,
+    public current_status: OrderStatus,
+    public isPaid: boolean,
+    public sellerId: string,
+    public userId: string,
+    public address: Address,
+    public expected_delivery_date: Date,
+  ) {}
+
+  static fromObject(id: string, other: any): Order {
+    return new Order(
+      id,
+      other.product,
+      other.quantity,
+      other.current_status,
+      other.isPaid,
+      other.sellerId,
+      other.userId,
+      Address.fromObject(other.address),
+      other.expected_delivery_date,
+    );
+  }
+
+  toPureJsObject() {
+    return Object.assign({}, this, { address: this.address.toPureJsObject() });
+  }
+}
